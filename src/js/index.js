f5d7a146e41124af26d47a01c8c6242115fb9a8e
Vinitoider <viniciusfamilia085@gmail.com>
2023-11-10 18:14:25 -0300

    responsividade e menu hamburguer

---
+++ src/js/index.js
@@ -0,0 +1,33 @@
+class MobileNavBar {
+    constructor (mobileMenu, navList, navLinks) {
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass = "active";
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.navList.classList.toggle(this.activeClass)
+        this.mobileMenu.classList.toggle(this.activeClass)
+    }
+
+    addClickEvent() {
+        this.mobileMenu.addEventListener("click", () => this.handleClick())
+    }
+
+    init() {
+        if (this.mobileMenu) {
+            this.addClickEvent();
+        }
+        return this;
+    }
+}
+
+const mobileNavbar = new MobileNavBar (
+    ".menu-mobile",
+    ".menu",
+    ".menu li" ,
+)
+mobileNavbar.init();
